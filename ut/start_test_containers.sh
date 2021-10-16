fc1d46f472d5f9e6dbede81afee9f5812c48ecdb
Oleg Volchkov <oleg@volchkov.net>
2021-10-16 09:41:47 +0300

    Добавил поддержку arm64-контейнеров для тестов

--- ut/start_test_containers.sh
+++ ut/start_test_containers.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
-MYSQL_IMAGE_TAG="5.7"
-POSTGRES_IMAGE_TAG="13"
+MYSQL_IMAGE="mysql/mysql-server:5.7"
+if [[ `uname -m` == 'arm64' ]]; then
+  MYSQL_IMAGE="beercan1989/arm-mysql:5.7"
+fi
+
+POSTGRES_IMAGE="postgres:13"
 
 # sudo docker pull mysql/mysql-server:$MYSQL_IMAGE_TAG
 sudo docker run --rm --name=pf2-ut-mysql -p 8306:3306 \
     -e MYSQL_USER=test \
     -e MYSQL_PASSWORD=test \
     -e MYSQL_DATABASE=mysql_test \
-    -d mysql/mysql-server:$MYSQL_IMAGE_TAG
+    -d "$MYSQL_IMAGE"
 
 # sudo docker pull postgres:$POSTGRES_IMAGE_TAG
 sudo docker run --rm --name=pf2-ut-postgres -p 8432:5432 \
     -e POSTGRES_USER=test \
     -e POSTGRES_PASSWORD=test \
     -e POSTGRES_DB=pg_test \
-    -d postgres:$POSTGRES_IMAGE_TAG \
+    -d "$POSTGRES_IMAGE" \
 && sleep 4 \
 && psql postgres://test:test@127.0.0.1:8432/pg_test -c 'create extension if not exists pgcrypto'
 
